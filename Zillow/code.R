3696e6898234a43206f34d36ef9a4554824d50cb
lizzzi111 <zinovyee@gmail.com>
2017-09-16 17:31:31 +0200

    code setwd

---
+++ Zillow/code.R
@@ -0,0 +1,8 @@
+if (Sys.info()[8] == "lizzzi111") {
+  setwd("~/Documents/DSG_2017_Finals/Zillow")
+} else if (Sys.info()[8] == ""){
+  setwd("~/Documents/DSG_2017_Finals/")
+} else if (Sys.info()[8] == ""){
+  setwd("~/Documents/DSG_2017_Finals/")
+}
+
