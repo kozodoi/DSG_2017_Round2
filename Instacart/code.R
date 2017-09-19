fe400bb4134f2fe2db7692223593040d8eaaf4b2
Nikita Kozodoi <n.kozodoi@icloud.com>
2017-09-19 20:42:33 +0200

    deleting Instacart folder

--- Instacart/code.R
+++
@@ -1,8 +0,0 @@
-if (Sys.info()[8] == "lizzzi111") {
-  setwd("~/Documents/DSG_2017_Finals/Zillow")
-} else if (Sys.info()[8] == ""){
-  setwd("~/Documents/DSG_2017_Finals/")
-} else if (Sys.info()[8] == ""){
-  setwd("~/Documents/DSG_2017_Finals/")
-}
-
--- Instacart/setwd.R
+++
@@ -1,8 +0,0 @@
-if (Sys.info()[8] == "lizzzi111") {
-  setwd("~/Documents/DSG_2017_Finals/Instacart/")
-} else if (Sys.info()[8] == "Margarita"){
-  setwd("/Users/Margarita/Documents/GitHub/DSG_2017_Finals/Instacart")
-} else if (Sys.info()[8] == ""){
-  setwd("~/Documents/DSG_2017_Finals/")
-}
-
