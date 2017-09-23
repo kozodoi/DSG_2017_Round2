06a177ea03bd6f1a05443d991d6fbe988afed1fe
Oleksiy Ostapenko <oleksiyostapenko@client195-108.wlan.hu-berlin.de>
2017-09-23 18:51:36 +0200

    baseline model function

--- functions/xg_boost_baseline.R
+++ functions/xg_boost_baseline.R
@@ -1,5 +1,27 @@
 
-base_line_model_xgboost = function(data_train, train_label, data_test, test_label, target, best_model=FALSE, nrounds = 100){
+base_line_model_xgboost = function(data_train, train_label, data_test, test_label, target, best_model=FALSE, nrounds = 100, type = "c"){
+  
+  
+  obje= "reg:linear"
+  metrics = list(eval_metric="error",
+                 eval_metric="rmse" 
+  )
+  best_model_parameter = "error"
+  
+  if(type == "c"){
+    obje= "binary:logistic"
+    metrics = list(eval_metric="error",
+                   eval_metric="rmse",
+                   eval_metric="auc"
+    )
+    best_model_parameter = "auc"
+    
+  }else if (type == "r"){
+    
+    obje = "reg:linear"
+    
+  }
+  
   
   size_train = dim(data_train)[1]
   data = rbind(data_train, data_test)
@@ -9,7 +31,7 @@ base_line_model_xgboost = function(data_train, train_label, data_test, test_labe
   
   dframe_treat_train = dframe_treat[1:size_train,] #train
   dframe_treat_test = dframe_treat[- c(1:size_train),] #test
-
+  
   
   #subset for now
   #data.train.xgboost_1 = dtrain[sample.int(n = nrow(dtrain), size = floor(.05*nrow(dtrain)), replace = F), ]
@@ -31,7 +53,10 @@ base_line_model_xgboost = function(data_train, train_label, data_test, test_labe
   
   watchlist <- list(train=DMMatrixTrain, test=DMMatrixTest)
   
+  
   rmseErrorsHyperparameters <- apply(searchGridSubCol, 1, function(parameterList){
+    
+    #searchGridSubCol= parameterList[1,]
     
     #Extract Parameters to test
     currentColsampleRate <- parameterList[["colsample_bytree"]]
@@ -40,23 +65,23 @@ base_line_model_xgboost = function(data_train, train_label, data_test, test_labe
     currentSubsample <- parameterList[["subsample"]]
     
     
-    param <- list(booster="gbtree", 
-                  objective="binary:logistic", 
-                  #eval_metric="mlogloss",
-                  #showsd = TRUE, 
-                  #metrics=metrics
-                  
-                  eval_metric="error",
-                  eval_metric="rmse", 
-                  eval_metric="auc",
-                  #num_class=2,
-                  eta = currentEta,
-                  colsample_bytree = currentColsampleRate,
-                  #gamma = 1,
-                  max_depth = currentMax_depth, 
-                  #min_child_weight = 1,
-                  subsample = currentSubsample
-                  #scale_pos_weight=scale_pos_weight
+    param <- c(list(booster="gbtree", 
+                    objective=obje,
+                    #eval_metric="mlogloss",
+                    #showsd = TRUE, 
+                    #metrics=metrics
+                    
+                    
+                    #num_class=2,
+                    eta = currentEta,
+                    colsample_bytree = currentColsampleRate,
+                    #gamma = 1,
+                    max_depth = currentMax_depth, 
+                    #min_child_weight = 1,
+                    subsample = currentSubsample
+                    #scale_pos_weight=scale_pos_weight),
+    ),
+    metrics
     )
     
     
@@ -71,36 +96,42 @@ base_line_model_xgboost = function(data_train, train_label, data_test, test_labe
     
     #Save eval_metric of the best iteration
     test_error = xgb2$evaluation_log[nrounds]$test_error
-    test_auc = xgb2$evaluation_log[nrounds]$test_auc
     test_rmse = xgb2$evaluation_log[nrounds]$test_rmse
     
     
-    return(c(test_error, test_auc, test_rmse, currentSubsample, currentColsampleRate, currentEta, currentMax_depth))
+    #test_auc = xgb2$evaluation_log[nrounds]$test_auc
+    
+    return(c(test_error, test_rmse, currentSubsample, currentColsampleRate, currentEta, currentMax_depth))
+    if(type == "c"){
+      test_auc = xgb2$evaluation_log[nrounds]$test_auc
+      return(c(test_error, test_rmse, test_auc, currentSubsample, currentColsampleRate, currentEta, currentMax_depth))
+      
+    }else if (type == "r"){
+      
+      return(c(test_error, test_rmse, currentSubsample, currentColsampleRate, currentEta, currentMax_depth))
+      
+    }
+    
     
   })
   
   rmseErrorsHyperparameters=transpose(as.data.frame(rmseErrorsHyperparameters))
-  names(rmseErrorsHyperparameters)=c(unlist(param[names(param)=="eval_metric"]),names(searchGridSubCol))
-  best.parameters= rmseErrorsHyperparameters[ which.max(rmseErrorsHyperparameters$auc),]
+  names(rmseErrorsHyperparameters)=c(unlist(unlist(metrics)),names(searchGridSubCol))
+  best.parameters= rmseErrorsHyperparameters[ which.min(rmseErrorsHyperparameters[[ best_model_parameter ]]),]
   
   fin_best_model=NULL
   if (best_model==TRUE){
-    param <- list(booster="gbtree", 
-                  objective="binary:logistic", 
-                  #eval_metric="mlogloss",
-                  #showsd = TRUE, 
-                  #metrics=metrics,
-                  eval_metric="error",
-                  eval_metric="rmse", 
-                  eval_metric="auc",
-                  #num_class=2,
-                  eta = best.parameters$eta,
-                  colsample_bytree = best.parameters$colsample_bytree,
-                  #gamma = 1,
-                  max_depth = best.parameters$max_depth, 
-                  #min_child_weight = 1,
-                  subsample = best.parameters$subsample
-    )
+    param <- c(list(booster="gbtree", 
+                    objective=obje,
+                    #num_class=2,
+                    eta = best.parameters$eta,
+                    colsample_bytree = best.parameters$colsample_bytree,
+                    #gamma = 1,
+                    max_depth = best.parameters$max_depth, 
+                    #min_child_weight = 1,
+                    subsample = best.parameters$subsample
+    ), 
+    metrics)
     
     
     fin_best_model <- xgb.train(data = DMMatrixTrain,
