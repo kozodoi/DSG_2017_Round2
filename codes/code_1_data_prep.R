4c97df8de2caac87eb991b0f27df67a05ee65ca4
nataliasverchkova <nsv@hellofresh.com>
2017-09-25 20:35:21 +0200

    natasha folder

--- codes/code_1_data_prep.R
+++ codes/code_1_data_prep.R
@@ -11,10 +11,10 @@
 rm(list = ls())
 
 # setting directory depending on a user
-if (Sys.info()[8] == "lizzzi111") {setwd("~/Documents/DSG_2017_Finals/")} 
-if (Sys.info()[8] == "kozodoi")   {setwd("~/Documents/Competitions/DSG_2017_Finals/")}
-if (Sys.info()[8] == "")          {setwd("")}
-if (Sys.info()[8] == "oleksiyostapenko"){setwd("/Users/oleksiyostapenko/Documents/HU_Berlin/ML/DSG/DSG_2017_Finals")}
+if (Sys.info()[8] == "lizzzi111")           {setwd("~/Documents/DSG_2017_Finals/")} 
+if (Sys.info()[8] == "kozodoi")             {setwd("~/Documents/Competitions/DSG_2017_Finals/")}
+if (Sys.info()[8] == "nataliasverchkova")   {setwd("~/Documents/DSG/DSG_2017_Finals/")}
+if (Sys.info()[8] == "oleksiyostapenko")    {setwd("/Users/oleksiyostapenko/Documents/HU_Berlin/ML/DSG/DSG_2017_Finals")}
 
 
 
