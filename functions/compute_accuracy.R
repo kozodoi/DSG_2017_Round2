91245d1db7b05f261354f934f7be931a882a1f0c
Nikita Kozodoi <n.kozodoi@icloud.com>
2017-09-26 11:11:55 +0200

    add compute_accuracy function draft

---
+++ functions/compute_accuracy.R
@@ -0,0 +1,11 @@
+compute_accuracy <- function(predictions, actuals) {
+  
+  # preventing errors
+  if (length(forecasts) != length(actuals))  {stop("Forecasts and actuals are not the same length")}
+  
+  # calculating accuracy
+  acc <- 
+  
+  # returning accuracy
+  return(acc)
+}
