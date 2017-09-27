1cf3d1ea9d5b78cd0b937a6811a47c8774e8748c
lizzzi111 <zinovyee@gmail.com>
2017-09-27 16:49:08 +0200

    adjusted pwd

--- codes/code_1_data_prep.R
+++ codes/code_1_data_prep.R
@@ -8,7 +8,7 @@
 rm(list = ls())
 
 # setting directory depending on a user
-if (Sys.info()[8] == "lizzzi111")           {setwd("~/DSG_2017_Finals/")} 
+if (Sys.info()[8] == "lizzzi111")           {setwd("~/Documents/DSG_2017_Finals/")} 
 if (Sys.info()[8] == "kozodoi")             {setwd("~/Documents/Competitions/DSG_2017_Finals/")}
 if (Sys.info()[8] == "nataliasverchkova")   {setwd("~/Documents/DSG/DSG_2017_Finals/")}
 if (Sys.info()[8] == "oleksiyostapenko")    {setwd("/Users/oleksiyostapenko/Documents/HU_Berlin/ML/DSG/DSG_2017_Finals")}
