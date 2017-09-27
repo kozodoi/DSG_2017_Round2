c70aa534de7a3722af0016d18e69c3e4161c9ed1
lizzzi111 <zinovyee@gmail.com>
2017-09-27 16:33:23 +0200

    add createModellib

---
+++ examples/createModelLib.R
@@ -0,0 +1,55 @@
+modelsLib = function(formula, train, valid, modellist = c("RF"), model_setup, 
+                     metric , model_control = NULL, preProcess = NULL) {
+  list.of.packages <- c("doParallel","foreach", "caret")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+  library(caret); library(foreach); library(doParallel)
+  
+  # Parallelization
+  runParallel = detectCores() -1
+  # Setup up parallel backend
+  ##### Setting up parallelization
+  cl <- makeCluster(min(detectCores()-1, runParallel))
+  #, outfile="") # This redirects the output to the R master console but not in RStudio
+  # on.exit(stopCluster(cl))
+  registerDoParallel(cl)
+  message(paste("\n Registered number of cores:\n",getDoParWorkers(),"\n"))
+  
+  start.time = Sys.time()
+
+  # Specify model estimation control settings, as given in the paper
+  if (is.null(model_control)) {
+    model_control = trainControl(method = "cv", index= data_train, indexOut = valid, verboseIter = TRUE,
+                                 savePredictions = FALSE, classProbs = TRUE, returnData = FALSE)
+  }
+  
+  
+  # Training use caret package
+  message("Start model training")
+  modelLibrary = list()
+  for (model in modellist) {
+    message(paste("Start training model", model, "at", Sys.time()))
+    modelObject = try(do.call(
+      caret::train, c(list(form = formula, 
+                           data = data, 
+                           trControl = model_control, 
+                           preProcess = preProcess,
+                           metric = metric), 
+                      
+                      model_setup[[model]])))
+    
+    if ("train" %in% class(modelObject)) {
+      modelLibrary[[model]] = modelObject
+    } else {
+      warning("Failed to train model ", model, ". ", modelObject)
+    }
+    rm(modelObject)
+  }
+  end.time = Sys.time()  # Print time
+  message(paste("Training time:", end.time - start.time))
+  
+  stopCluster(cl)
+  
+  return(modelLibrary)
+}
+
